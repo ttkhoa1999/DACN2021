ddd72a7196a51d8326be0857d204f7eed5f930af
Trần Thanh Khoa <62534711+ttkhoa1999@users.noreply.github.com>
2021-05-02 07:43:04 +0700

    Updates

---
+++ precache-manifest.a1504d1e0d6ad5a30877f04bd6bb91dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd7d2fe2b87cdd03d113111c322f444",
+    "url": "/DACN2021/index.html"
+  },
+  {
+    "revision": "8c06b3415b0eb3f64ece",
+    "url": "/DACN2021/static/css/main.50556752.chunk.css"
+  },
+  {
+    "revision": "e8783b215a1b8d7a60fa",
+    "url": "/DACN2021/static/js/2.e01fb1b7.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/DACN2021/static/js/2.e01fb1b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c06b3415b0eb3f64ece",
+    "url": "/DACN2021/static/js/main.8b8ba5f5.chunk.js"
+  },
+  {
+    "revision": "d01677ba25712c49bbec",
+    "url": "/DACN2021/static/js/runtime-main.1f8a9a9d.js"
+  },
+  {
+    "revision": "e1ab07f9d67850ceffedaaa35a61294f",
+    "url": "/DACN2021/static/media/icon.e1ab07f9.png"
+  },
+  {
+    "revision": "e601c2e2a5ff40044c75775fbbb73c4e",
+    "url": "/DACN2021/static/media/logo.e601c2e2.jpg"
+  },
+  {
+    "revision": "1c8ae16eb001faa4b2bc256d7ace4f65",
+    "url": "/DACN2021/static/media/logo_dlu.1c8ae16e.png"
+  },
+  {
+    "revision": "8c142f49e3e621728b0af81741503f16",
+    "url": "/DACN2021/static/media/new.8c142f49.png"
+  }
+]);
